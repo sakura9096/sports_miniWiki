b9411027848733d8e4dd58b2708b26bd10cd0e07
sakura9096 <yue.zhang.ivy@gmail.com>
2015-12-07 15:23:34 -0500

    Background and Build Path

--- sports_miniWiki/src/sports_miniWiki/Sports_miniWiki.java
+++ sports_miniWiki/src/sports_miniWiki/Sports_miniWiki.java
@@ -49,8 +49,8 @@ public class Sports_miniWiki {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 640, 383);
+		frame = new JFrame("Sports miniWiki");
+		frame.setBounds(100, 100, 1280, 740);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new CardLayout(0, 0));
 		
@@ -64,14 +64,20 @@ public class Sports_miniWiki {
 		FIFA.setLayout(null);
 		
 		JButton btnHome = new JButton("Home");
+		btnHome.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
 		btnHome.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				FIFA.setVisible(false);
 				Main_window.setVisible(true);
 			}
 		});
-		btnHome.setBounds(517, 25, 117, 29);
+		btnHome.setBounds(25, 25, 120, 40);
 		FIFA.add(btnHome);
+		
+		JLabel lblFifabackground = new JLabel("FIFA_background");
+		lblFifabackground.setIcon(new ImageIcon("/Users/yuezhang/Documents/sports_miniWiki/sports_miniWiki/src/resources/FIFA_background.jpg"));
+		lblFifabackground.setBounds(0, 0, 1280, 720);
+		FIFA.add(lblFifabackground);
 		FIFA.setVisible(false);
 		
 		final JPanel NBA = new JPanel();
@@ -79,14 +85,20 @@ public class Sports_miniWiki {
 		NBA.setLayout(null);
 		
 		JButton btnHome_1 = new JButton("Home");
+		btnHome_1.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
 		btnHome_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				NBA.setVisible(false);
 				Main_window.setVisible(true);
 			}
 		});
-		btnHome_1.setBounds(503, 28, 117, 29);
+		btnHome_1.setBounds(25, 25, 120, 40);
 		NBA.add(btnHome_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setIcon(new ImageIcon(this.getClass().getResource("/resources/NBA_trophy.jpg")));
+		lblNewLabel_1.setBounds(0, 0, 1280, 720);
+		NBA.add(lblNewLabel_1);
 		NBA.setVisible(false);
 		
 		final JPanel ATP_WTA = new JPanel();
@@ -94,13 +106,14 @@ public class Sports_miniWiki {
 		ATP_WTA.setLayout(null);
 		
 		JButton btnHome_2 = new JButton("Home");
+		btnHome_2.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
 		btnHome_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ATP_WTA.setVisible(false);
 				Main_window.setVisible(true);
 			}
 		});
-		btnHome_2.setBounds(506, 31, 117, 29);
+		btnHome_2.setBounds(25, 25, 120, 40);
 		ATP_WTA.add(btnHome_2);
 		ATP_WTA.setVisible(false);
 		
@@ -111,8 +124,8 @@ public class Sports_miniWiki {
 				Main_window.setVisible(false);
 			}
 		});
-		btnFifa.setFont(new Font("Lucida Grande", Font.BOLD, 16));
-		btnFifa.setBounds(74, 167, 140, 50);
+		btnFifa.setFont(new Font("Lucida Grande", Font.BOLD, 20));
+		btnFifa.setBounds(200, 520, 200, 80);
 		Main_window.add(btnFifa);
 		
 		JButton btnNba = new JButton("NBA");
@@ -122,8 +135,8 @@ public class Sports_miniWiki {
 				Main_window.setVisible(false);
 			}
 		});
-		btnNba.setFont(new Font("Lucida Grande", Font.BOLD, 16));
-		btnNba.setBounds(254, 167, 140, 50);
+		btnNba.setFont(new Font("Lucida Grande", Font.BOLD, 20));
+		btnNba.setBounds(540, 520, 200, 80);
 		Main_window.add(btnNba);
 		
 		
@@ -135,20 +148,14 @@ public class Sports_miniWiki {
 				Main_window.setVisible(false);
 			}
 		});
-		btnAtpWta.setFont(new Font("Lucida Grande", Font.BOLD, 16));
-		btnAtpWta.setBounds(436, 167, 140, 50);
+		btnAtpWta.setFont(new Font("Lucida Grande", Font.BOLD, 20));
+		btnAtpWta.setBounds(880, 520, 200, 80);
 		Main_window.add(btnAtpWta);
 		
-		JLabel lblSportsMiniwiki = new JLabel("Sports miniWiki");
-		lblSportsMiniwiki.setForeground(Color.YELLOW);
-		lblSportsMiniwiki.setFont(new Font("Lucida Grande", Font.BOLD, 24));
-		lblSportsMiniwiki.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSportsMiniwiki.setBounds(197, 58, 243, 51);
-		Main_window.add(lblSportsMiniwiki);
-		
 		JLabel lblNewLabel = new JLabel("background");
-		lblNewLabel.setIcon(new ImageIcon("/Users/yuezhang/Documents/sports_miniWiki/sports_miniWiki/SPORTS-background.jpg"));
-		lblNewLabel.setBounds(0, 0, 640, 360);
+		lblNewLabel.setIcon(new ImageIcon(this.getClass().getResource("/resources/Sports_background.jpg")));
+		lblNewLabel.setSize(720, 480);
+		lblNewLabel.setBounds(0, 0, 1280, 720);
 		Main_window.add(lblNewLabel);
 
 	}
